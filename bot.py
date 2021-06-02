8b077f05084f370d1ffce1b98b88f5b76449902e
rfgbnfycvjkktn <rfgbnfycvjkktn@gmail.com>
2021-06-02 16:17:17 +0500

    new

--- bot.py
+++ bot.py
@@ -5,17 +5,34 @@ import pytz
 import json
 import traceback
 
-P_TIMEZONE = pytz.timezone(config.TIMEZONE)
-TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
+#P_TIMEZONE = pytz.timezone(config.TIMEZONE)
+#TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
 
 bot = telebot.TeleBot(config.TOKEN)
+#bot.polling(none_stop=True)
+
+@bot.message_handler(commands=["start", "help"])
+def start_command(message):
+    bot.send_message(message.chat.id,'Тест')
+    # bot.reply_to(message, "Howdy, how are you doing?")
+
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     bot.reply_to(message, "Howdy, how are you doing?")
+
 bot.polling(none_stop=True)
 
 
-@bot.message_handler(commands=['start'])
-def start_command(message):
-    bot.send_message(
-        message.chat.id,
-        'Тест.\n' +
-        'Чтобы получить помощь нажми /help.'
-    )
+# import telebot
+#
+# bot = telebot.TeleBot("TOKEN")
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+# 	bot.reply_to(message, "Howdy, how are you doing?")
+#
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+# 	bot.reply_to(message, message.text)
+#
+# bot.polling()
